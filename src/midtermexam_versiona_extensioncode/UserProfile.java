532db88908df6ea300bb5398290fc19c3123fa75
ricosar <122638957+ricosar@users.noreply.github.com>
2023-02-24 12:38:52 -0500

    adding comments to the code
    added random comments for question #5

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -12,6 +12,7 @@ package midtermexam_versiona_extensioncode;
  * Students are reminded to add themselves as modifiers when editing the
  * code.
  * @author dancye, 2019
+ Random comments for question #5 on midterm!
  */
 public class UserProfile 
 {
